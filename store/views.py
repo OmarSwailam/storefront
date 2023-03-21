a27546381650a0711e340ce39bb442823e852b04
Omar Swailam <omarswailam@outlook.com>
2023-03-21 22:52:01 +0200

    cbv

--- store/views.py
+++ store/views.py
@@ -3,45 +3,52 @@ from django.db.models.aggregates import Count
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from .models import Product, Collection, OrderItem
 from .serializers import ProductSerializer, CollectionSerializer
 
 
-@api_view(['GET', 'POST'])
-def product_list(request):
-    if request.method == 'GET':
+class ProductList(APIView):
+    def get(self, request):
         queryset = Product.objects.all()
         serializer = ProductSerializer(
             queryset, many=True)
         return Response(serializer.data)
 
-    if request.method == 'POST':
+    def post(self, request):
         serializer = ProductSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def product_detail(request, pk):
-    product = get_object_or_404(Product, id=pk)
-    if request.method == 'GET':
+class ProductDetail(APIView):
+    def get_object(self, pk):
+        return get_object_or_404(Product, id=pk)
+
+    def get(self, request, pk):
+        product = product = self.get_object(pk)
         serializer = ProductSerializer(product)
         return Response(serializer.data)
-    if request.method == "PUT":
+
+    def put(self, request, pk):
+        product = self.get_object(pk)
         serializer = ProductSerializer(product, data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
 
-    if request.method == 'DELETE':
+    def delete(self, request, pk):
+        product = self.get_object(pk)
         if product.orderitems.count():
-            return Response({'error': 'cannot delete the Product because it\'s associated with an OrderItem'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+            return Response(
+                {'error': 'cannot delete the Product because it\'s associated with an OrderItem'},
+                status=status.HTTP_405_METHOD_NOT_ALLOWED)
         product.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-@api_view(['GET', 'POST'])
+@ api_view(['GET', 'POST'])
 def collection_list(request):
     if request.method == 'GET':
         queryset = Collection.objects.annotate(
@@ -57,7 +64,7 @@ def collection_list(request):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-@api_view(['GET', 'PUT', 'DELETE'])
+@ api_view(['GET', 'PUT', 'DELETE'])
 def collection_detail(request, pk):
     collection = get_object_or_404(Collection.objects.annotate(
         products_count=Count('products')).all(), pk=pk)

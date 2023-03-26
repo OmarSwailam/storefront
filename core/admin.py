fbb1eedf99233e8d3cdf0cb1cb3f7ce3f7147b50
Omar Swailam <omarswailam@outlook.com>
2023-03-26 14:22:47 +0200

    extend user

---
+++ core/admin.py
@@ -0,0 +1,40 @@
+from django.contrib import admin
+from django.contrib.contenttypes.admin import GenericTabularInline
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from store.models import Product
+from store.admin import ProductAdmin
+from tags.models import TaggedItem
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "username",
+                    "email",
+                    "password1",
+                    "password2",
+                    "first_name",
+                    "last_name",
+                ),
+            },
+        ),
+    )
+
+
+class TagInline(GenericTabularInline):
+    model = TaggedItem
+    autocomplete_fields = ["tag"]
+
+
+class CustomProductAdmin(ProductAdmin):
+    inlines = [TagInline]
+
+
+admin.site.unregister(Product)
+admin.site.register(Product, CustomProductAdmin)
